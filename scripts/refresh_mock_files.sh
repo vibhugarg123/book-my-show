55b09cc121279464cc36768e9045825c8b1f10a3
Vibhu Garg <Vibhu.Garg@gojek.com>
2021-02-10 23:45:31 +0530

    modified refresh mocks script

--- scripts/refresh_mock_files.sh
+++ scripts/refresh_mock_files.sh
@@ -1,13 +1,10 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 #This expects file names to be with/without underscore only and refreshes mocks which were earlier generated.
 MOCK_FILES=$(find . -name "*mock.go")
-echo "Mocks to be refreshed for "$MOCK_FILES
-
-for PATH in $MOCK_FILES; do
-  PACKAGE_NAME="$(echo $PATH | cut -d '/' -f2)"
-  MOCK_FILE_NAME="$(echo $PATH | rev | cut -d '/' -f1 | rev)"
+for PATH_MOCK_FILE in $MOCK_FILES; do
+  PACKAGE_NAME="$(echo $PATH_MOCK_FILE | cut -d '/' -f2)"
+  MOCK_FILE_NAME="$(echo $PATH_MOCK_FILE | rev | cut -d '/' -f1 | rev)"
   FILE_NAME="$(echo $MOCK_FILE_NAME | sed 's/_mock//g')"
-  echo "Mock generation : " " for package name: " $PACKAGE_NAME " mock file name: " $MOCK_FILE_NAME " mock generated for : " $FILE_NAME
-  echo "Executing...mockgen --source=$FILE_NAME --destination=$PATH --package=$PACKAGE_NAME"
-  mockgen --source=$FILE_NAME --destination=$PATH --package=$PACKAGE_NAME
+  echo "refreshing mockgen --source=$FILE_NAME --destination=$PATH_MOCK_FILE --package=$PACKAGE_NAME"
+  ~/go/bin/mockgen --source=$FILE_NAME --destination=$PATH_MOCK_FILE --package=$PACKAGE_NAME
 done
